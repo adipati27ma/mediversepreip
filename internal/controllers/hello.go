28b68e33198442584f2f4d2bb8e1136f076b962e
adipati27ma <adipati.cs.samsung@gmail.com>
2024-09-10 11:27:16 +0700

    init: initial commit

---
+++ internal/controllers/hello.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"github.com/sev-2/raiden"
+)
+
+type HelloWorldRequest struct { // Add your request data
+}
+
+type HelloWorldResponse struct {
+	Message string `json:"message"`
+}
+
+type HelloWorldController struct {
+	raiden.ControllerBase
+	Http    string `path:"/hello" type:"custom"`
+	Payload *HelloWorldRequest
+	Result  HelloWorldResponse
+}
+
+func (c *HelloWorldController) Get(ctx raiden.Context) error {
+	c.Result.Message = "Welcome to raiden"
+	return ctx.SendJson(c.Result)
+}
+
+func (c *HelloWorldController) Post(ctx raiden.Context) error {
+	c.Result.Message = "Data has been inserted."
+	return ctx.SendJson(c.Result)
+}
